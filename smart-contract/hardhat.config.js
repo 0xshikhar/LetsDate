2f6c5251e92a026d0ac4582d892312714bfcef0b
Shikhar Singh <64684053+linux369@users.noreply.github.com>
2022-03-28 04:18:53 +0530

    error fixed

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -2,14 +2,14 @@ require("@nomiclabs/hardhat-waffle");
 require('dotenv').config({path: '.env'})
 
 const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
+const POLYGON_PRIVATE_KEY = process.env.POLYGON_PRIVATE_KEY
 
 module.exports = {
   solidity: '0.8.4',
   networks: {
-    rinkeby: {
-      url: ALCHEMY_API_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
+    matic: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/_g11-TeFdVQO58t2alykJadgA2BZwbVY",
+      accounts: [POLYGON_PRIVATE_KEY],
     },
   },
 }
